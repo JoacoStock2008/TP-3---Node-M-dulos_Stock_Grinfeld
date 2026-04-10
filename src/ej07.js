7ad395fd9fac8ded71ad3e4a0edfa42fe2501ae4
JOAQUIN STOCK <48795975@ort>
2026-04-10 12:09:09 -0300

    seguimos igual

--- src/ej07.js
+++ src/ej07.js
@@ -1,21 +1,5 @@
-function mostrarFin(segundos){
-    setTimeout(() => {
-        console.log("Fin del contador");
-    }, segundos*1000)
+const tiempoTotal = 10
+let timepoIntervalo;
+function contador{
+    
 }
-mostrarFin(10);
-
-function incrementar(contador){
-    contador++;
-}
-function mostrarContador(segundos){
-    let contador = 1;
-    const intervalo = setInterval(() => {
-        console.log(contador);
-        incrementar(contador);
-        if(contador == 10) {clearInterval(intervalo); console.clear()}
-    }, 1000)
-}
-mostrarContador(10);
-mostrarFin(10);
-//Seguir el ej7 y terminar el 8 y 9 HOY
